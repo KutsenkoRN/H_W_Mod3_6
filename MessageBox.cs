1ea817d007da64f075ad3fcf86ab5b3e8813252d
Roman Kutsenko <73640616+KutsenkoRN@users.noreply.github.com>
2023-08-17 11:43:32 +0300

    Add files via upload

---
+++ MessageBox.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H_W_Mod3_6
+{
+    internal class MessageBox
+    {
+        public event EventHandler<State> OnWinClosed;
+        public async void Open()
+        {
+            Console.WriteLine("Window is opened.");
+            await Task.Delay(300);
+            Console.WriteLine("window was closed by the user.");
+            OnWinClosed?.Invoke(this, StateOnClosed());
+        }
+        public enum State
+        {
+            Ok,
+            Cancel
+        }
+
+        public static State StateOnClosed()
+        {
+            Random rand = new Random();
+            int i = rand.Next(0, 100);
+            if (i < 50)
+            {
+                return State.Ok;
+            }
+            else
+            {
+                return State.Cancel;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+namespace H_W_Mod3_6
+{
+    internal class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            MessageBox messageBox = new MessageBox();
+            var taskCompletionSource = new TaskCompletionSource();
+            messageBox.OnWinClosed += (messageBox, state) =>
+            {
+                if (state == MessageBox.State.Ok)
+                {
+                    Console.WriteLine("the operation is confirmed");
+                }
+                if (state == MessageBox.State.Cancel)
+                {
+                    Console.WriteLine("the operation is canceled");
+                }
+                taskCompletionSource.SetResult();
+            };
+
+            messageBox.Open();
+            await taskCompletionSource.Task;
+            Console.WriteLine("****************");
+        }
+    }
+}
